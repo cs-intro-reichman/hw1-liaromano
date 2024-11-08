2dda1d86e46dd8c1f65e0721bb8da36f797e70f6
liaromano <lia.romano@post.runi.ac.il>
2024-11-08 17:10:48 +0200

    HW1

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,21 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lin= Integer.parseInt(args[0]);
+		int a=(int) (Math.random()*lin);
+		int b=(int) (Math.random()*lin);
+		int c=(int) (Math.random()*lin);
+		System.out.println(a+ " "+b+" "+c);
+		int max= Math.max(a, b);
+		int min= Math.min(a,b);
+		int middle= c;
+		middle= Math.min(max, middle);
+		middle= Math.max(min,middle);
+		max= Math.max(max,c);
+		min= Math.min(min,c);
+
+System.out.println(min+" "+middle+" "+max);
 	}
 }
+
+
